75ea2724a82a033f0e7828acaedf967b3df1fa2a
JckD <c16335383@mydit.ie>
2021-02-16 19:43:25 +0000

    working with semi dynaimoc devices

--- lightControl.js
+++ lightControl.js
@@ -1,33 +1,81 @@
-const { Client } = require('tplink-smarthome-api');
-const { jscolor } = require('@eastdesire/jscolor');
-
-const client = new Client();
-
+require('dotenv').config()
+const { login } = require("tplink-cloud-api");
+const jscolor = require('@eastdesire/jscolor');
 
 
 
-client.startDiscovery()
-console.log(client.devices)
-console.log(client.devices.values())
+async function getDevices()  {
+    //console.log('test')
+    const tplink = await login(process.env.TPLINK_USER, process.env.TPLINK_PW) 
+    let deviceList  = await tplink.getDeviceList();
 
-let values = client.devices.values()
+    console.log(deviceList);
 
-console.log(values.Bulb)
-console.log(values.next().value)
+    let bigLight = await tplink.getLB130("BigLight")
+
+    console.log(await bigLight.isOn());
+    let deviceTable = document.getElementById('deviceTableBody')
+    //console.log(deviceTable)
+    console.log(deviceList[0])
+    for(i in deviceList) {
+
+        let rowCount = deviceTable.rows.length;
+        let row = deviceTable.insertRow(rowCount);
+        
+        // Add device name cell  from devices list alias
+        let deviceNameCell = row.insertCell(0);
+        deviceNameCell.innerHTML = deviceList[i].alias
+
+        // Add power switch cell
+        let powerCell = row.insertCell(1)
+        let checkboxDiv = document.createElement('div');
+        checkboxDiv.className = "custom-switch"
+
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.id="switch-"+i;
+
+        if (await bigLight.isOn()) {
+            checkbox.checked = 'checked';
+        } else {
+            checkbox.checked = '';
+        }
+        
+        checkbox.onchange = function(){bulbToggle(bigLight)};
+        let label = document.createElement('label');
+        label.htmlFor = checkbox.id
+
+        checkboxDiv.appendChild(checkbox)
+        checkboxDiv.appendChild(label)
+        powerCell.appendChild(checkboxDiv)
+
+        let colorCell = row.insertCell(2);
+        
+        let button = document.createElement('button');
+        let pickerConfig = {
+            backgroundColor : '#333',
+            palletteCols : 5,
+            onChange : 'bulbColour(this, "BigLight")',
+        };
+        var picker = new jscolor(button, pickerConfig);
+        colorCell.appendChild(button)
+        //colorCell.innerHTML = '<button  data-jscolor="{pickerConfig}" id="colorInput"">'
+
+        jscolor.install()
+
+    }
+}
 
 // Toggle the bulb on/off
-// currently hardcoded to single bulb
-function bulbToggle() {
-    //get the bulb from IP
-    let device = client.getDevice({ host: '192.168.0.164' }).then( async (device) => {
-        // Toggle Bulb on off
-        device.setPowerState(!await device.getPowerState())  
-    }) 
+function bulbToggle(bulb) {  
+    bulb.toggle()
 }
 
 // Handle bulb changing colour from picker
-function bulbColour(picker) {
-    console.log('test')
-   // document.getElementById
+async function bulbColour(picker, bulb) {
+    const tplink = await login(process.env.TPLINK_USER, process.env.TPLINK_PW) 
+    let deviceList  = await tplink.getDeviceList();
+
+    let bigLight = await tplink.getLB130(bulb).setState(1, 75, picker.channels.h, picker.channels.s, 0 )
 }
 
