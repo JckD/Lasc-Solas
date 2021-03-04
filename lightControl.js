a31411c1ac0e6d0ce6f84d1274885a35e94a2c75
JckD <c16335383@mydit.ie>
2021-03-04 01:10:21 +0000

    added device off at source msg

--- lightControl.js
+++ lightControl.js
@@ -37,10 +37,8 @@ function getInfo() {
 // TODO check if the use has been remember'd first and have modal
 // show accodingly
 function openModal(){
-  
     if (store.get('user') != undefined) {
         
-
         location.href = '#'
         getDevices()
     } else {
@@ -58,7 +56,13 @@ async function getDevices()  {
     } else {
         user = store.get('user')
     }
-    const tplink = await login(user.email, user.pw)
+    let tplink;
+    try {
+        tplink = await login(user.email, user.pw)
+    } catch {
+        console.log('device off')
+    }
+    
     if (tplink){
         document.getElementById('deviceTable').classList.remove('invisible');
         document.getElementById('deviceTable').classList.add('visible');
@@ -69,16 +73,27 @@ async function getDevices()  {
 
     let bigLight = await tplink.getLB130("BigLight")
 
-    //get table 
-    let deviceTable = document.getElementById('deviceTable');
-    // create a body for the table and append it
-    let deviceTableBody = document.createElement('tbody');
-    deviceTableBody.id = 'deviceTableBody';
-    deviceTable.appendChild(deviceTableBody)
+
+    // if there's no devices
+    if (deviceList.length == 0) {
+        
+        document.getElementById('noDevicesDiv').classList.remove('invisible');
+        document.getElementById('noDevicesDiv').classList.add('visible');
+        
+    } else {
+        document.getElementById('noDevicesDiv').classList.remove('visible');
+        document.getElementById('noDevicesDiv').classList.add('invisible');
+        //get table 
+        let deviceTable = document.getElementById('deviceTable');
+        // create a body for the table and append it
+        let deviceTableBody = document.createElement('tbody');
+        deviceTableBody.id = 'deviceTableBody';
+        deviceTable.appendChild(deviceTableBody)
+    }
 
     // Create cells for every device in the list
     for(i in deviceList) {
-
+        //console.log(bigLight)
         let rowCount = deviceTableBody.rows.length;
         let row = deviceTableBody.insertRow(rowCount);
         
@@ -95,30 +110,64 @@ async function getDevices()  {
         checkbox.type = "checkbox";
         checkbox.id="switch-"+i;
 
-        if (await bigLight.isOn()) {
+        let deviceOn;
+        
+        // Check if device is on at the source and show appropriate error msg if not
+        try {
+        deviceOn = await bigLight.isOn()
+        }  catch (e) {
+            deviceOn = false
+        }
+
+        if (deviceOn) {
+            
+            checkbox.onchange = function(){bulbToggle(bigLight, i)};
+            let label = document.createElement('label');
+            label.htmlFor = checkbox.id
+    
+            checkboxDiv.appendChild(checkbox)
+            checkboxDiv.appendChild(label)
+            powerCell.appendChild(checkboxDiv)
+        } else {
+            let deviceOffDiv = document.createElement('div');
+            deviceOffDiv.id = 'errorDiv';
+            deviceOffDiv.classList.add('alert');
+            deviceOffDiv.classList.add('alert-secondary');
+            deviceOffDiv.setAttribute('role', 'alert')
+            deviceOffDiv.innerText = 'Device off at source'
+            powerCell.appendChild(deviceOffDiv)
+
+            let emptyCell1 = row.insertCell(2);
+            let emptyCell2 = row.insertCell(3);
+        }
+            
+    
+       
+           
+            
+            
+        
+        if (deviceOn) {
             checkbox.checked = 'checked';
         } else {
             checkbox.checked = '';
         }
         
-        checkbox.onchange = function(){bulbToggle(bigLight, i)};
-        let label = document.createElement('label');
-        label.htmlFor = checkbox.id
-
-        checkboxDiv.appendChild(checkbox)
-        checkboxDiv.appendChild(label)
-        powerCell.appendChild(checkboxDiv)
-
+      
         let colorCell = row.insertCell(2);
-        
-        let lightState = await bigLight.getState()
-        
+        let lightState
+        try {
+            lightState = await bigLight.getState()
+        } catch (e) {
+            console.log('light off')
+        }
        
-
+        // get the current state of the bulb and set it as the colour
         let color = rgb(lightState.hue, lightState.saturation, lightState.brightness)
         let deviceName = deviceList[i].alias
 
         let button = document.createElement('button');
+        // create the config for the colour picker
         let pickerConfig = {
             backgroundColor : '#333',
             onChange : 'bulbColour(this)',
@@ -126,21 +175,24 @@ async function getDevices()  {
         };
         var picker = new jscolor(button, pickerConfig);
         colorCell.appendChild(button)
-        //colorCell.innerHTML = '<button  data-jscolor="{pickerConfig}" id="colorInput"">'
-
+      
+        //instantiate colour picker
         jscolor.install()
 
         // add wave button
         let waveButtonCell = row.insertCell(3);
         let waveDiv = document.createElement('div');
         waveDiv.className = "custom-switch"
-
         let waveButton = document.createElement('input')
         waveButton.type = "checkbox";
+
+        //set the id of the button to start at 50 to not clash with on/off switches bit of a hack
         waveButton.id="switch-"+(50 + i);
         let waving = false
         waveButton.innerHTML = 'start'
         let wavfunc;
+
+        // set the onchange function of the wave button passing the ligth obj, wavefunc var, if the button is checked or not, and the index of the device
         waveButton.onchange = function(){slowColorChange(bigLight, wavfunc, waveButton.checked, i)}
         let waveLabel = document.createElement('label');
         waveLabel.htmlFor = waveButton.id
@@ -149,9 +201,6 @@ async function getDevices()  {
         waveButtonCell.appendChild(waveDiv)
 
     }
-
-    // if there's no devices
-   // if (deviceList.length )
 }
 
 // Toggle the bulb on/off
